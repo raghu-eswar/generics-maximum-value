55de5ba767cd0f2a76d0252174a2cf5602e17a41
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-06 18:31:03 +0530

    Refactored and created generic class Max.java

--- src/main/java/com/generics/Max.java
+++ src/main/java/com/generics/Max.java
@@ -1,6 +1,20 @@
 package com.generics;
 
-public class Max {
+public class Max <Any extends  Comparable> {
+    Any value1;
+    Any value2;
+    Any value3;
+
+    public Max(Any value1, Any value2, Any value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public Any maxOf(){
+        return maxOf(this.value1, this.value2, this.value3);
+    }
+
 
     public static <Any extends  Comparable> Any maxOf(Any arg1, Any arg2, Any arg3) {
         Any max = arg1;
--- src/test/java/com/generics/MaxTest.java
+++ src/test/java/com/generics/MaxTest.java
@@ -7,55 +7,55 @@ public class MaxTest {
 
     @Test
     public void maxIntegerAtBeginning() {
-        Integer result = Max.maxOf(82,37,68);
-        Assert.assertEquals(82, result.intValue());
+        Max<Integer> max = new Max<>(82, 37,68);
+        Assert.assertEquals(82, max.maxOf().intValue());
     }
 
     @Test
     public void maxIntegerAtSecond() {
-        Integer result = Max.maxOf(22,82,68);
-        Assert.assertEquals(82, result.intValue());
+        Max<Integer> max = new Max<>(32, 82,68);
+        Assert.assertEquals(82, max.maxOf().intValue());
     }
 
     @Test
     public void maxIntegerAtLast() {
-        Integer result = Max.maxOf(42,37,68);
-        Assert.assertEquals(68, result.intValue());
+        Max<Integer> max = new Max<>(22, 37,82);
+        Assert.assertEquals(82, max.maxOf().intValue());
     }
 
     @Test
     public void maxFloatValueAtBeginning() {
-        Float result = Max.maxOf(30.3f, 20.2f,10.1f);
-        Assert.assertEquals(30.3f, result, 0.0f);
+        Max<Float> max = new Max<>(30.3f, 20.2f,10.1f);
+        Assert.assertEquals(30.3f, max.maxOf(), 0.0f);
     }
 
     @Test
     public void maxFloatValueAtSecond() {
-        Float result = Max.maxOf(10.1f, 30.3f,20.2f);
-        Assert.assertEquals(30.3f, result, 0.0f);
+        Max<Float> max = new Max<>(20.2f, 30.3f,10.1f);
+        Assert.assertEquals(30.3f, max.maxOf(), 0.0f);
     }
 
     @Test
     public void maxFloatValueAtLast() {
-        Float result = Max.maxOf(10.1f, 20.2f,30.3f);
-        Assert.assertEquals(30.3f, result, 0.0f);
+        Max<Float> max = new Max<>(10.1f, 20.2f,30.3f);
+        Assert.assertEquals(30.3f, max.maxOf(), 0.0f);
     }
 
     @Test
     public void maxStringAtBeginning() {
-        String result = Max.maxOf("peanch", "apple", "bananna");
-        Assert.assertEquals("peanch", result);
+        Max<String> max = new Max<>("peanch", "apple", "bananna");
+        Assert.assertEquals("peanch", max.maxOf());
     }
 
     @Test
     public void maxStringAtSecond() {
-        String result = Max.maxOf("apple", "peanch", "bananna");
-        Assert.assertEquals("peanch", result);
+        Max<String> max = new Max<>("apple", "peanch", "bananna");
+        Assert.assertEquals("peanch", max.maxOf());
     }
 
     @Test
     public void maxStringAtLast() {
-        String result = Max.maxOf("apple", "bananna", "peanch");
-        Assert.assertEquals("peanch", result);
+        Max<String> max = new Max<>("apple", "bananna", "peanch");
+        Assert.assertEquals("peanch", max.maxOf());
     }
 }
