02c0fd033d2ad2ef2164da454c0ed11689ca7066
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-06 17:39:55 +0530

    Added feature to find maximum Integer

--- src/main/java/com/generics/Max.java
+++ src/main/java/com/generics/Max.java
@@ -2,4 +2,12 @@ package com.generics;
 
 public class Max {
 
+    public static Integer maxOf(Integer arg1, Integer arg2, Integer arg3) {
+        Integer max = arg1;
+        if (max.compareTo(arg2) < 0)
+            max = arg2;
+        if (max.compareTo(arg3) < 0)
+            max = arg3;
+        return max;
+    }
 }
--- src/test/java/com/generics/MaxTest.java
+++ src/test/java/com/generics/MaxTest.java
@@ -1,5 +1,25 @@
 package com.generics;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaxTest {
 
+    @Test
+    public void maxIntegerAtBeginning() {
+        Integer result = Max.maxOf(82,37,68);
+        Assert.assertEquals(82, result.intValue());
+    }
+
+    @Test
+    public void maxIntegerAtSecond() {
+        Integer result = Max.maxOf(22,82,68);
+        Assert.assertEquals(82, result.intValue());
+    }
+
+    @Test
+    public void maxIntegerAtLast() {
+        Integer result = Max.maxOf(42,37,68);
+        Assert.assertEquals(68, result.intValue());
+    }
 }
